9a6db97e8690722c4bf1e7928172a2ffec1e5dcd
Steve Robinson <ssrobins@gmail.com>
2018-11-17 12:13:02 -0800

    Export variables from config.txt so they will be available for conanfile.py

--- build_ios_sim.sh
+++ build_ios_sim.sh
@@ -3,6 +3,7 @@ set -e
 cd $(dirname "$0")
 
 source config.txt
+export $(cut -d= -f1 config.txt)
 
 conan create . ${package_user}/${package_channel} -s os=iOS -s os.version=8.0
 conan upload ${package_name}/${package_version}@${package_user}/${package_channel} -r ${package_repo} --all
--- build_mac.sh
+++ build_mac.sh
@@ -3,6 +3,7 @@ set -e
 cd $(dirname "$0")
 
 source config.txt
+export $(cut -d= -f1 config.txt)
 
 conan create . ${package_user}/${package_channel}
 conan upload ${package_name}/${package_version}@${package_user}/${package_channel} -r ${package_repo} --all
