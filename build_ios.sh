837d90b4494aa20e2301e771cffc670dc0c5a627
Steve Robinson <ssrobins@gmail.com>
2018-11-17 21:50:54 -0800

    Initial commit of build scripts and .gitattributes

---
+++ build_ios.sh
@@ -0,0 +1,9 @@
+set -e
+
+cd $(dirname "$0")
+
+. ./config.txt
+export $(cut -d= -f1 config.txt)
+
+conan create . ${package_user}/${package_channel} -s os=iOS -s arch=armv7 -s os.version=8.0
+conan upload ${package_name}/${package_version}@${package_user}/${package_channel} -r ${package_repo} --all
---
+++ build_ios_sim.sh
@@ -0,0 +1,9 @@
+set -e
+
+cd $(dirname "$0")
+
+. ./config.txt
+export $(cut -d= -f1 config.txt)
+
+conan create . ${package_user}/${package_channel} -s os=iOS -s os.version=8.0
+conan upload ${package_name}/${package_version}@${package_user}/${package_channel} -r ${package_repo} --all
