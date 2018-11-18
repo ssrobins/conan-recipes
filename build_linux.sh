968316ccd9e4ab53c2d64e48c61475230c2fe601
Steve Robinson <ssrobins@gmail.com>
2018-11-17 21:28:43 -0800

    Initial commit of build scripts and .gitattributes

---
+++ build_linux.sh
@@ -0,0 +1,9 @@
+set -e
+
+cd $(dirname "$0")
+
+. ./config.txt
+export $(cut -d= -f1 config.txt)
+
+conan create . ${package_user}/${package_channel}
+conan upload ${package_name}/${package_version}@${package_user}/${package_channel} -r ${package_repo} --all
