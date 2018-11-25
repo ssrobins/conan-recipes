f27ea32e3d3cb5babdca108f2aecd94c4b23ebc5
Steve Robinson <ssrobins@gmail.com>
2018-11-25 11:20:27 -0800

    Package pdbs

--- conanfile.py
+++ conanfile.py
@@ -64,6 +64,8 @@ class Conan(ConanFile):
     def package(self):
         cmake = self.configure_cmake()
         cmake.install()
+        if self.settings.compiler == 'Visual Studio':
+            self.copy(pattern="*.pdb", dst="lib", src=".")
         
     def package_info(self):
         self.cpp_info.includedirs = [os.path.join('include', 'freetype2')]
