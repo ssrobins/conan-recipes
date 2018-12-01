5862db83bed6557234f89ffa6cfae4e12d9b65b4
Steve Robinson <ssrobins@gmail.com>
2018-12-01 14:21:59 -0800

    Make sure zlibstatic is the only zlib packaged on Windows (other platforms to follow)

--- conanfile.py
+++ conanfile.py
@@ -66,13 +66,15 @@ class Conan(ConanFile):
     def package(self):
         self.copy("*.h", dst="include", src=self.source_subfolder)
         self.copy("*.h", dst="include", src=self.build_folder, keep_path=False)
-        self.copy("*.lib", dst="lib", keep_path=False)
+        self.copy("build/lib/zlibstatic.lib", dst="lib", keep_path=False)
+        # Be sure to do zlibstatic on non-Windows platforms too!
         self.copy("*.a", dst="lib", keep_path=False)
         if self.settings.compiler == 'Visual Studio':
             self.copy(pattern="*.pdb", dst="lib", src=".")
 
     def package_info(self):
         if self.settings.os == "Windows" and not tools.os_info.is_linux:
-            self.cpp_info.libs = ['zlib']
+            self.cpp_info.libs = ['zlibstatic']
         else:
+            # Be sure to do zlibstatic on non-Windows platforms too!
             self.cpp_info.libs = ['z']
