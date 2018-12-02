1bcb8881f27e8e917a3a7d47582c995ddce74714
Steve Robinson <ssrobins@gmail.com>
2018-12-01 21:21:11 -0800

    Package *NIX libs

--- conanfile.py
+++ conanfile.py
@@ -52,6 +52,7 @@ class Conan(ConanFile):
     def package(self):
         self.copy("bzlib.h", dst="include", src=self.zip_folder_name)
         self.copy("*.lib", dst="lib", keep_path=False)
+        self.copy("*.a", dst="lib", keep_path=False)
         if self.settings.compiler == 'Visual Studio':
             self.copy(pattern="*.pdb", dst="lib", src=".")
 
