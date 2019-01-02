a80210750412a28a2dd3d123bdbaf81576933559
Steve Robinson <ssrobins@gmail.com>
2019-01-01 18:22:25 -0800

    Package debug libs

--- conanfile.py
+++ conanfile.py
@@ -1,6 +1,6 @@
 from conans import ConanFile, CMake, tools
 import os
-
+from cmake_utils import cmake_init, cmake_build_debug_release, cmake_install_debug_release
 
 class Conan(ConanFile):
     name = "libpng"
@@ -9,8 +9,9 @@ class Conan(ConanFile):
     homepage = "http://www.libpng.org"
     license = "Libpng http://www.libpng.org/pub/png/src/libpng-LICENSE.txt"
     url = "https://gitlab.com/ssrobins/conan-" + name
-    settings = "os", "compiler", "build_type", "arch"
+    settings = "os", "compiler", "arch"
     generators = "cmake"
+    exports = "cmake_utils.py"
     exports_sources = ["CMakeLists.txt"]
     zip_folder_name = "libpng-%s" % version
     zip_name = "%s.tar.gz" % zip_folder_name
@@ -28,40 +29,20 @@ class Conan(ConanFile):
         os.unlink(self.zip_name)
         os.rename(self.zip_folder_name, self.source_subfolder)
 
-    def configure_cmake(self):
-        generator = None
-        if self.settings.os == "Macos" or self.settings.os == "iOS":
-            generator = "Xcode"
-        cmake = CMake(self, generator=generator)
-        if self.settings.os == "Android":
-            cmake.definitions["CMAKE_SYSTEM_NAME"] = "Android"
-            cmake.definitions["CMAKE_SYSTEM_VERSION"] = os.getenv("android_sdk_version")
-            cmake.definitions["CMAKE_ANDROID_ARCH_ABI"] = os.getenv("android_arch_abi")
-            cmake.definitions["CMAKE_ANDROID_NDK"] = os.environ['ANDROID_HOME'] + "/android-ndk-" + os.getenv("android_ndk_version")
-            cmake.definitions["CMAKE_ANDROID_NDK_TOOLCHAIN_VERSION"] = "clang"
-            cmake.definitions["CMAKE_ANDROID_STL_TYPE"] = "c++_static"
-        if self.settings.os == "iOS":
-            cmake.definitions["CMAKE_TOOLCHAIN_FILE"] = os.path.join(self.build_folder, "ios.toolchain.cmake")
-            cmake.definitions["ENABLE_BITCODE"] = "FALSE"
-            if self.settings.arch == "x86_64":
-                cmake.definitions["IOS_PLATFORM"] = "SIMULATOR64"
-            else:
-                cmake.definitions["IOS_ARCH"] = "armv7"
-        cmake.configure(build_dir=self.build_subfolder)
-        return cmake
-
     def build(self):
-        cmake = self.configure_cmake()
-        cmake.build()
+        cmake = cmake_init(self.settings, CMake(self), self.build_folder)
+        cmake_build_debug_release(cmake, self.build_subfolder)
 
     def package(self):
-        cmake = self.configure_cmake()
-        cmake.install()
-        if self.settings.compiler == 'Visual Studio':
-            self.copy(pattern="*.pdb", dst="lib", src=".", keep_path=False)
+        cmake = cmake_init(self.settings, CMake(self), self.build_folder)
+        cmake_install_debug_release(cmake, self.build_subfolder)
+        if self.settings.compiler == "Visual Studio":
+            self.copy(pattern="*.pdb", dst="lib", src="build/source/png_static.dir/Release", keep_path=False)
 
     def package_info(self):
         if self.settings.os == "Windows":
-            self.cpp_info.libs = ['libpng16_static']
+            self.cpp_info.debug.libs = ["libpng16_staticd"]
+            self.cpp_info.release.libs = ["libpng16_static"]
         else:
-            self.cpp_info.libs = ['png']
+            self.cpp_info.debug.libs = ["pngd"]
+            self.cpp_info.release.libs = ["png"]
