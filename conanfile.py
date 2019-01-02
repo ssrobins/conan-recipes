e9f23f3f51bcf9a15f65bff2bc6f63611d5d153e
Steve Robinson <ssrobins@gmail.com>
2019-01-01 23:02:36 -0800

    Add scripts to build and package Box2D

---
+++ conanfile.py
@@ -0,0 +1,41 @@
+from conans import ConanFile, CMake, tools
+import os, shutil
+from cmake_utils import cmake_init, cmake_build_debug_release, cmake_install_debug_release
+
+class Conan(ConanFile):
+    name = "box2d"
+    version = os.getenv("package_version")
+    description = "A 2D physics engine for games"
+    homepage = "https://box2d.org/"
+    license = "Zlib"
+    url = "https://gitlab.com/ssrobins/conan-" + name
+    settings = "os", "compiler", "arch"
+    generators = "cmake"
+    exports = "cmake_utils.py"
+    exports_sources = ["CMakeLists.txt"]
+    zip_folder_name = "Box2D-%s" % version
+    zip_name = "v%s.tar.gz" % version
+    build_subfolder = "build"
+    source_subfolder = "source"
+    
+    def source(self):
+        tools.download("https://gitlab.com/ssrobins/cmake-utils/raw/master/global_settings.cmake", "global_settings.cmake")
+        tools.download("https://gitlab.com/ssrobins/cmake-utils/raw/master/ios.toolchain.cmake", "ios.toolchain.cmake")
+        tools.download("https://github.com/erincatto/Box2D/archive/%s" % self.zip_name, self.zip_name)
+        tools.unzip(self.zip_name)
+        os.unlink(self.zip_name)
+        os.rename(self.zip_folder_name, self.source_subfolder)
+
+    def build(self):
+        cmake = cmake_init(self.settings, CMake(self), self.build_folder)
+        cmake_build_debug_release(cmake, self.build_subfolder)
+
+    def package(self):
+        cmake = cmake_init(self.settings, CMake(self), self.build_folder)
+        cmake_install_debug_release(cmake, self.build_subfolder) 
+        if self.settings.compiler == "Visual Studio":
+            self.copy(pattern="*.pdb", dst="lib", src="build/source/Box2D.dir/Release", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.debug.libs = ["Box2Dd"]
+        self.cpp_info.release.libs = ["Box2D"]
