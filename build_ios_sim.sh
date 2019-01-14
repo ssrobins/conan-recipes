f27c3a93326e5e50862b336464ecd314136359b8
Steve Robinson <ssrobins@gmail.com>
2019-01-13 22:10:34 -0800

    Remove scripts that aren't needed until mobile support is added

--- build_ios_sim.sh
+++
@@ -1,9 +0,0 @@
-set -e
-
-cd $(dirname "$0")
-
-. ./config.txt
-export $(cut -d= -f1 config.txt)
-
-conan create . ${package_user}/${package_channel} -s os=iOS -s os.version=8.0
-conan upload ${package_name}/${package_version}@${package_user}/${package_channel} -r ${package_repo} --all
