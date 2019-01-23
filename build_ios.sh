6e2ccd1f20a85bd3b0c3a4abb8b6dc6833208c17
Steve Robinson <ssrobins@gmail.com>
2019-01-22 21:22:49 -0800

    Fix if statement syntax on the rest of the scripts

--- build_ios.sh
+++ build_ios.sh
@@ -8,6 +8,6 @@ export package_channel=$(git rev-parse --abbrev-ref HEAD)
 
 conan create . ${package_user}/${package_channel} -s os=iOS -s arch=armv7 -s os.version=8.0
 
-if [[ "$CI" == "true" ]]; then
+if [ "$CI" = "true" ]; then
     conan upload ${package_name}/${package_version}@${package_user}/${package_channel} -r ${package_repo} --all
 fi
