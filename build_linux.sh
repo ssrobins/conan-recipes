fd6ed9ff6d5db77c7e8a395dd601891861f922a9
Steve Robinson <ssrobins@gmail.com>
2019-01-22 21:12:01 -0800

    Fix if statement syntax

--- build_linux.sh
+++ build_linux.sh
@@ -8,6 +8,6 @@ export package_channel=$(git rev-parse --abbrev-ref HEAD)
 
 conan create . ${package_user}/${package_channel} -s compiler.libcxx=libstdc++11
 
-if [[ "$CI" == "true" ]]; then
+if [ "$CI" = "true" ]; then
     conan upload ${package_name}/${package_version}@${package_user}/${package_channel} -r ${package_repo} --all
 fi
