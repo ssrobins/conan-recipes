9aa02bb4084ccadac97ffca4ad24ed11c5503372
Steve Robinson <ssrobins@gmail.com>
2019-01-23 21:44:56 -0800

    Only upload the conan package when run in the CI. Use branch name for package channel in the CI, otherwise use 'testing'

--- build_windows.bat
+++ build_windows.bat
@@ -5,8 +5,17 @@ cd /d %~dp0
 
 for /f "delims=" %%x in (config.txt) do (set "%%x")
 
+if defined CI_COMMIT_REF_NAME (
+    set package_channel=%CI_COMMIT_REF_NAME%
+) else (
+    set package_channel=testing
+)
+
 conan create . %package_user%/%package_channel% -s arch=x86 -s compiler.runtime=MT || goto :error
-conan upload %package_name%/%package_version%@%package_user%/%package_channel% -r %package_repo% --all || goto :error
+
+if "%CI%" == "true" (
+    conan upload %package_name%/%package_version%@%package_user%/%package_channel% -r %package_repo% --all || goto :error
+)
 
 :error
 exit /b %errorlevel%
