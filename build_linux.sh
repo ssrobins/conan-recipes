7f905382b4dab0d0bce0830d071f2c8cf94461b5
Steve Robinson <ssrobins@gmail.com>
2019-01-23 21:31:01 -0800

    Only upload the conan package when run in the CI. Use branch name for package channel in the CI, otherwise use 'testing'

--- build_linux.sh
+++ build_linux.sh
@@ -5,5 +5,14 @@ cd $(dirname "$0")
 . ./config.txt
 export $(cut -d= -f1 config.txt)
 
+if [ -n "$CI_COMMIT_REF_NAME" ]; then
+    package_channel=$CI_COMMIT_REF_NAME
+else
+    package_channel=testing
+fi
+
 conan create . ${package_user}/${package_channel} -s compiler.libcxx=libstdc++11
-conan upload ${package_name}/${package_version}@${package_user}/${package_channel} -r ${package_repo} --all
+
+if [ "$CI" = "true" ]; then
+    conan upload ${package_name}/${package_version}@${package_user}/${package_channel} -r ${package_repo} --all
+fi
