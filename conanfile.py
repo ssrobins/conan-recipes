476d8ccd6b22ca5f49ab44898203729dec623168
Steve Robinson <ssrobins@gmail.com>
2019-01-23 22:14:57 -0800

    Use stable packages

--- conanfile.py
+++ conanfile.py
@@ -19,7 +19,7 @@ class Conan(ConanFile):
     source_subfolder = "source"
     
     def requirements(self):
-        self.requires.add("sdl2/2.0.8@stever/testing")
+        self.requires.add("sdl2/2.0.8@stever/stable")
 
     def source(self):
         tools.download("https://gitlab.com/ssrobins/cmake-utils/raw/master/global_settings.cmake", "global_settings.cmake")
