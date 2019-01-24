10299e7c25dd306dba0b6abd7300a18f203040c3
Steve Robinson <ssrobins@gmail.com>
2019-01-23 21:41:39 -0800

    Only upload the conan package when run in the CI. Use branch name for package channel in the CI, otherwise use 'testing'

--- build_android.sh
+++ build_android.sh
@@ -5,5 +5,14 @@ cd $(dirname "$0")
 . ./config.txt
 export $(cut -d= -f1 config.txt)
 
+if [ -n "$CI_COMMIT_REF_NAME" ]; then
+    package_channel=$CI_COMMIT_REF_NAME
+else
+    package_channel=testing
+fi
+
 conan create . ${package_user}/${package_channel} -s os=Android -s os.api_level=${android_sdk_version} -s arch=armv7 -s compiler=clang -s compiler.version=7.0 -s compiler.libcxx=libstdc++11
-conan upload ${package_name}/${package_version}@${package_user}/${package_channel} -r ${package_repo} --all
+
+if [ "$CI" = "true" ]; then
+    conan upload ${package_name}/${package_version}@${package_user}/${package_channel} -r ${package_repo} --all
+fi
