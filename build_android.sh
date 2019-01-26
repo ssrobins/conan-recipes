b3faeb6d73aaac22e93e31d1f04f09818f306bf5
Steve Robinson <ssrobins@gmail.com>
2019-01-25 18:59:46 -0800

    When not in CI, still use the branch name as the package channel

--- build_android.sh
+++ build_android.sh
@@ -8,7 +8,7 @@ export $(cut -d= -f1 config.txt)
 if [ -n "$CI_COMMIT_REF_NAME" ]; then
     package_channel=$CI_COMMIT_REF_NAME
 else
-    package_channel=testing
+    package_channel=$(git rev-parse --abbrev-ref HEAD)
 fi
 
 conan create . ${package_user}/${package_channel} -s os=Android -s os.api_level=${android_sdk_version} -s arch=armv7 -s compiler=clang -s compiler.version=7.0 -s compiler.libcxx=libstdc++11
