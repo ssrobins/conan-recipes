19125e4b017cff651a5fcddf9febc03e2a76ca41
Steve Robinson <ssrobins@gmail.com>
2019-03-07 20:42:58 -0800

    Get conan username from a file

--- build_mac.sh
+++ build_mac.sh
@@ -2,10 +2,7 @@ set -e
   
 cd $(dirname "$0")
 
-if [ -z "$CONAN_USERNAME" ]; then
-    echo "Set CONAN_USERNAME environment variable and re-run"
-    exit 1
-fi
+CONAN_USERNAME=$(cat conan_user.txt)
 
 if [ -z "$CI_COMMIT_REF_NAME" ]; then
     package_channel=$(git rev-parse --abbrev-ref HEAD)
--- build_windows.bat
+++ build_windows.bat
@@ -3,16 +3,13 @@ setlocal
 
 cd /d %~dp0
 
-if not defined CONAN_USERNAME (
-    echo Set CONAN_USERNAME environment variable and re-run
-    goto :error
-)
+set /p CONAN_USERNAME=<conan_user.txt
 
 if not defined CI_COMMIT_REF_NAME (
     for /f "usebackq tokens=*" %%b in (`git rev-parse --abbrev-ref HEAD`) do (set CI_COMMIT_REF_NAME=%%b)
 )
 
-conan create . %CONAN_USERNAME%/%CI_COMMIT_REF_NAME% -s arch=x86 -s compiler.runtime=MT
+conan create . %CONAN_USERNAME%/%CI_COMMIT_REF_NAME% -s arch=x86 -s compiler.version=15 -s compiler.runtime=MT
 
 :error
 exit /b %errorlevel%
