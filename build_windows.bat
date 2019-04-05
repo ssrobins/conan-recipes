f82663ef2fd6413a59d6ab0e97d8e2e34c7d8279
Steve Robinson <ssrobins@gmail.com>
2019-04-04 21:57:45 -0700

    Upgrade to VS2019

--- build_windows.bat
+++ build_windows.bat
@@ -9,7 +9,7 @@ if not defined CI_COMMIT_REF_NAME (
     for /f "usebackq tokens=*" %%b in (`git rev-parse --abbrev-ref HEAD`) do (set CI_COMMIT_REF_NAME=%%b)
 )
 
-conan create . %CONAN_USERNAME%/%CI_COMMIT_REF_NAME% -s arch=x86 -s compiler.version=15 -s compiler.runtime=MT
+conan create . %CONAN_USERNAME%/%CI_COMMIT_REF_NAME% -s arch=x86 -s compiler.version=16 -s compiler.runtime=MT
 
 :error
 exit /b %errorlevel%
