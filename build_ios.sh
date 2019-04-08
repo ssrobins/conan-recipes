6aebecf671ee0832e52011d330e1ac86daccb691
Steve Robinson <ssrobins@gmail.com>
2019-04-07 22:04:22 -0700

    Change CONAN_USERNAME to CONAN_USER

--- build_ios.sh
+++ build_ios.sh
@@ -2,10 +2,10 @@ set -e
 
 cd $(dirname "$0")
 
-CONAN_USERNAME=$(cat conan_user.txt)
+CONAN_USER=$(cat conan_user.txt)
 
 if [ -z "$CI_COMMIT_REF_NAME" ]; then
     CI_COMMIT_REF_NAME=$(git rev-parse --abbrev-ref HEAD)
 fi
 
-conan create . $CONAN_USERNAME/$CI_COMMIT_REF_NAME -s os=iOS -s arch=armv7 -s os.version=8.0
+conan create . $CONAN_USER/$CI_COMMIT_REF_NAME -s os=iOS -s arch=armv7 -s os.version=8.0
