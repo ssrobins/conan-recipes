711e50d39d42f0b2b254dc6bb6f34a9f4c9b9496
Steve Robinson <ssrobins@gmail.com>
2019-04-07 22:09:25 -0700

    Change CONAN_USERNAME to CONAN_USER

--- build_android-v7a.sh
+++ build_android-v7a.sh
@@ -2,10 +2,10 @@ set -e
 
 cd $(dirname "$0")
 
-CONAN_USERNAME=$(cat conan_user.txt)
+CONAN_USER=$(cat conan_user.txt)
 
 if [ -z "$CI_COMMIT_REF_NAME" ]; then
     CI_COMMIT_REF_NAME=$(git rev-parse --abbrev-ref HEAD)
 fi
 
-conan create . $CONAN_USERNAME/$CI_COMMIT_REF_NAME -s os=Android -s os.api_level=$android_sdk_version -s arch=armv7 -s compiler=clang -s compiler.version=8 -s compiler.libcxx=libc++
+conan create . $CONAN_USER/$CI_COMMIT_REF_NAME -s os=Android -s os.api_level=$android_sdk_version -s arch=armv7 -s compiler=clang -s compiler.version=8 -s compiler.libcxx=libc++
