64ed881cc547405a8b0339e4fa63a256376ee2e3
Steve Robinson <ssrobins@gmail.com>
2019-04-07 22:08:08 -0700

    Change CONAN_USERNAME to CONAN_USER

--- build_mac.sh
+++ build_mac.sh
@@ -2,10 +2,10 @@ set -e
   
 cd $(dirname "$0")
 
-CONAN_USERNAME=$(cat conan_user.txt)
+CONAN_USER=$(cat conan_user.txt)
 
 if [ -z "$CI_COMMIT_REF_NAME" ]; then
     CI_COMMIT_REF_NAME=$(git rev-parse --abbrev-ref HEAD)
 fi
 
-conan create . $CONAN_USERNAME/$CI_COMMIT_REF_NAME
+conan create . $CONAN_USER/$CI_COMMIT_REF_NAME
