00d4501d83e48fdbc53271a0af00bc32635b283f
Steve Robinson <ssrobins@gmail.com>
2019-04-07 21:01:23 -0700

    Add the cmake utilities to source rather than download them so changes result in a commit

--- conanfile.py
+++ conanfile.py
@@ -12,15 +12,13 @@ class Conan(ConanFile):
     settings = "os", "compiler", "arch"
     generators = "cmake"
     exports = "cmake_utils.py"
-    exports_sources = ["CMakeLists.txt", "CMakeLists-%s.txt" % name]
+    exports_sources = ["CMakeLists.txt", "CMakeLists-%s.txt" % name, "global_settings.cmake", "ios.toolchain.cmake"]
     zip_folder_name = "%s-%s" % (name, version)
     zip_name = "%s.tar.gz" % zip_folder_name
     build_subfolder = "build"
     source_subfolder = "source"
 
     def source(self):
-        tools.download("https://gitlab.com/ssrobins/cmake-utils/raw/master/global_settings.cmake", "global_settings.cmake")
-        tools.download("https://gitlab.com/ssrobins/cmake-utils/raw/master/ios.toolchain.cmake", "ios.toolchain.cmake")
         tools.download("http://dnqpy.com/libs/%s" % self.zip_name, self.zip_name)
         tools.unzip(self.zip_name)
         os.unlink(self.zip_name)
