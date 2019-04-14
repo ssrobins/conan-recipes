d1c084d35c6e9c2639625731b42dfd004514eb40
Steve Robinson <ssrobins@gmail.com>
2019-04-13 18:39:04 -0700

    Package the debug PDBs to fix debug build errors when no other copies of them are already on the computer. Add 'd' suffix to debug PDBs and store them in the lib directory so they are easily packaged.

--- conanfile.py
+++ conanfile.py
@@ -44,8 +44,7 @@ class Conan(ConanFile):
         if self.settings.os == "Android":
             self.copy("*.java", dst="android", src=os.path.join(self.source_subfolder, "android-project", "app", "src", "main", "java", "org", "libsdl", "app"))
         elif self.settings.compiler == "Visual Studio":
-            self.copy(pattern="*.pdb", dst="lib", src="build/source/SDL2-static.dir/Release", keep_path=False)
-            self.copy(pattern="*.pdb", dst="lib", src="build/source/SDL2main.dir/Release", keep_path=False)
+            self.copy("*.pdb", dst="lib", keep_path=False)
 
     def package_info(self):
         self.cpp_info.includedirs = [os.path.join("include", "SDL2")]
