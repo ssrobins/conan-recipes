a60a4f73a9d419838ed1587402965a705a1caa71
Steve Robinson <ssrobins@gmail.com>
2019-04-14 10:36:06 -0700

    Upgrade iOS toolchain

--- global_settings.cmake
+++ global_settings.cmake
@@ -57,10 +57,6 @@ elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
 elseif(ANDROID)
     set(CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} -g0")
     set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -g0")
-elseif(IOS)
-    add_link_options(
-        "${XCODE_IOS_PLATFORM_VERSION_FLAGS}"
-    )
 else()
     add_compile_options(
         -Werror # Warning as error
