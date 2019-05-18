3ba183eb7104667d49e64651dfcaecb37b475740
Steve Robinson <ssrobins@gmail.com>
2019-05-17 22:53:10 -0700

    Get release libs out of debug builds

--- conanfile.py
+++ conanfile.py
@@ -39,7 +39,6 @@ class Conan(ConanFile):
     def package_info(self):
         self.cpp_info.debug.libs = ["gmockd", "gmock_maind", "gtestd", "gtest_maind"]
         self.cpp_info.release.libs = ["gmock", "gmock_main", "gtest", "gtest_main"]
-        self.cpp_info.libs = self.cpp_info.release.libs
         if self.settings.os == "Linux":
             system_libs = ["pthread"]
             self.cpp_info.debug.libs.extend(system_libs)
