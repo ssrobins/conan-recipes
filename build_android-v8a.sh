1346b32da96dd2ec53bc8a9cd6182f31989fb283
Steve Robinson <ssrobins@gmail.com>
2019-06-07 22:22:09 -0700

    Set Android 'api_level' to minSdkVersion so Conan sets the --target flag properly

--- build_android-v8a.sh
+++ build_android-v8a.sh
@@ -8,4 +8,4 @@ if [ -z "$CI_COMMIT_REF_NAME" ]; then
     CI_COMMIT_REF_NAME=$(git rev-parse --abbrev-ref HEAD)
 fi
 
-conan create . $CONAN_USER/$CI_COMMIT_REF_NAME -s os=Android -s os.api_level=$android_sdk_version -s arch=armv8 -s compiler=clang -s compiler.version=8 -s compiler.libcxx=libc++
+conan create . $CONAN_USER/$CI_COMMIT_REF_NAME -s os=Android -s os.api_level=21 -s arch=armv8 -s compiler=clang -s compiler.version=8 -s compiler.libcxx=libc++
