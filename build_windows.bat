b91a5c5e61802ea010fb9033fc3d93a4d74cf043
Steve Robinson <ssrobins@gmail.com>
2019-08-03 17:24:21 -0700

    Remove user and channel from conan create command across all platforms

--- build_windows.bat
+++ build_windows.bat
@@ -3,13 +3,4 @@ setlocal
 
 cd /d %~dp0
 
-set /p CONAN_USER=<conan_user.txt
-
-if not defined CI_COMMIT_REF_NAME (
-    for /f "usebackq tokens=*" %%b in (`git rev-parse --abbrev-ref HEAD`) do (set CI_COMMIT_REF_NAME=%%b)
-)
-
-conan create . %CONAN_USER%/%CI_COMMIT_REF_NAME% -s arch=x86 -s compiler.version=16 -s compiler.runtime=MT
-
-:error
-exit /b %errorlevel%
+conan create . -s arch=x86 -s compiler.version=16 -s compiler.runtime=MT
