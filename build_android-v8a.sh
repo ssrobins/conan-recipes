ed1c8f571e040aecc4d785de176e6fe530c0a08f
Steve Robinson <ssrobins@gmail.com>
2019-08-04 11:48:07 -0700

    Check the conan remote for updates to make sure the required package revision is available

--- build_android-v8a.sh
+++ build_android-v8a.sh
@@ -2,4 +2,4 @@ set -e
 
 cd $(dirname "$0")
 
-conan create . -s os=Android -s os.api_level=21 -s arch=armv8 -s compiler=clang -s compiler.version=8 -s compiler.libcxx=libc++
+conan create --update . -s os=Android -s os.api_level=21 -s arch=armv8 -s compiler=clang -s compiler.version=8 -s compiler.libcxx=libc++
--- build_ios.sh
+++ build_ios.sh
@@ -2,4 +2,4 @@ set -e
 
 cd $(dirname "$0")
 
-conan create . -s os=iOS -s arch=armv7 -s os.version=8.0
+conan create --update . -s os=iOS -s arch=armv7 -s os.version=8.0
