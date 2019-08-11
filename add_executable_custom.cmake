cc7b7c88260901885b339aaa05e9ba3f0dd37b4f
Steve Robinson <ssrobins@gmail.com>
2019-08-10 17:52:26 -0700

    Set the RESOURCE target property in the same file as the icon file list

--- add_executable_custom.cmake
+++ add_executable_custom.cmake
@@ -6,8 +6,7 @@ function(add_executable_custom target_name)
     endif()
 
     if(APPLE)
-        set_target_properties(${target_name}
-            PROPERTIES
+        set_target_properties(${target_name} PROPERTIES
             MACOSX_BUNDLE_BUNDLE_VERSION "${version_major}.${version_minor}.${version_patch}"
             MACOSX_BUNDLE_SHORT_VERSION_STRING "${version_major}.${version_minor}.${version_patch}"
             MACOSX_BUNDLE_INFO_PLIST "${CMAKE_SOURCE_DIR}/cmake/info.plist.in"
@@ -23,19 +22,16 @@ function(add_executable_custom target_name)
             set(xcode_bitcode NO)
         endif()
 
-        set_target_properties(${target_name}
-            PROPERTIES
+        set_target_properties(${target_name} PROPERTIES
             MACOSX_BUNDLE TRUE
             XCODE_ATTRIBUTE_ASSETCATALOG_COMPILER_APPICON_NAME "AppIcon"
             XCODE_ATTRIBUTE_ASSETCATALOG_COMPILER_LAUNCHIMAGE_NAME "LaunchImage"
             XCODE_ATTRIBUTE_ENABLE_BITCODE "${xcode_bitcode}"
             XCODE_ATTRIBUTE_INSTALL_PATH "$(LOCAL_APPS_DIR)"
             XCODE_ATTRIBUTE_TARGETED_DEVICE_FAMILY "1,2" # 1=iPhone/iPod, 2=iPad
-            RESOURCE "${icons}"
         )
     else(APPLE)
-        set_target_properties(${target_name}
-            PROPERTIES
+        set_target_properties(${target_name} PROPERTIES
             XCODE_ATTRIBUTE_CODE_SIGN_STYLE "Manual"
         )
     endif()
