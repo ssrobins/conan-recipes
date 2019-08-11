f5cff2cb438ba9a1c0b0f68a835376189f26c5d0
Steve Robinson <ssrobins@gmail.com>
2019-08-10 21:35:34 -0700

    Move conan remote setting to individual scripts so it'll 'just work'

--- build_windows.bat
+++ build_windows.bat
@@ -3,4 +3,6 @@ setlocal
 
 cd /d %~dp0
 
+conan remote add bintray-stever https://api.bintray.com/conan/stever/conan --insert --force
+
 conan create --update . -s arch=x86 -s compiler.version=16 -s compiler.runtime=MT
