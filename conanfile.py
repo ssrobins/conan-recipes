797952a216af93a8b5bd1b419771bee34e4cb93b
Steve Robinson <ssrobins@gmail.com>
2019-09-07 23:11:32 -0700

    Remove the reference to the iOS toolchain which is no longer used

--- conanfile.py
+++ conanfile.py
@@ -15,7 +15,7 @@ class Conan(ConanFile):
     generators = "cmake"
     revision_mode = "scm"
     exports = "cmake_utils.py"
-    exports_sources = ["CMakeLists.diff", "CMakeLists.txt", "global_settings.cmake", "ios.toolchain.cmake"]
+    exports_sources = ["CMakeLists.diff", "CMakeLists.txt", "global_settings.cmake"]
     zip_folder_name = "SDL2-%s" % version
     zip_name = "%s.tar.gz" % zip_folder_name
     build_subfolder = "build"
