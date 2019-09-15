e46a68f902881f7e9135a4982340361cb68a63c1
Steve Robinson <ssrobins@gmail.com>
2019-09-15 11:03:06 -0600

    Fix packaging on iOS so it's only grabbing the universal binaries. Make gtest a build requirement since it's only used for the unit tests and isn't a dependency for anyone using the package.

--- conanfile.py
+++ conanfile.py
@@ -24,8 +24,10 @@ class Conan(ConanFile):
     ]
     build_subfolder = "build"
 
+    def build_requirements(self):
+        self.build_requires.add("gtest/1.8.1#0ab3cafdcd7fe85deaefe41b3011d36fbd86e38e")
+
     def requirements(self):
-        self.requires.add("gtest/1.8.1#0ab3cafdcd7fe85deaefe41b3011d36fbd86e38e")
         self.requires.add("sdl2/2.0.8#4841c177e6f3920faa36e56495bfcd23f55d1d1e")
         self.requires.add("sdl2_image/2.0.5#d05b5cff4d08b2726485af58b25217dd4fca0668")
         self.requires.add("sdl2_ttf/2.0.15#55cd97129e10c9f54ff98e3896df593c333881ae")
@@ -37,7 +39,7 @@ class Conan(ConanFile):
     def package(self):
         self.copy("*.h", dst="include", keep_path=False)
         self.copy("*.a", dst="lib", keep_path=False)
-        self.copy("*.lib", dst="lib", keep_path=False)
+        self.copy("build/lib/*.a", dst="lib", keep_path=False)
         if self.settings.compiler == "Visual Studio":
             self.copy("*.pdb", dst="lib", keep_path=False)
         if self.settings.os == "Android":
