e429f599c8c7350ba1edb0e501cfb81c23df0e84
Steve Robinson <ssrobins@gmail.com>
2019-11-03 13:01:14 -0800

    Get cmake_utils.py and global_settings.cmake from a Conan package

--- conanfile.py
+++ conanfile.py
@@ -1,6 +1,5 @@
 from conans import ConanFile, CMake, tools
 import os
-from cmake_utils import cmake_init, cmake_build_debug_release, cmake_install_debug_release
 
 class Conan(ConanFile):
     name = "sdl2"
@@ -14,12 +13,14 @@ class Conan(ConanFile):
     settings = "os", "compiler", "arch"
     generators = "cmake"
     revision_mode = "scm"
-    exports = "cmake_utils.py"
-    exports_sources = ["CMakeLists.diff", "CMakeLists.txt", "global_settings.cmake"]
+    exports_sources = ["CMakeLists.diff", "CMakeLists.txt"]
     zip_folder_name = "SDL2-%s" % version
     zip_name = "%s.tar.gz" % zip_folder_name
     build_subfolder = "build"
     source_subfolder = "source"
+
+    def build_requirements(self):
+        self.build_requires.add("cmake_utils/0.1.0#7f17deeced79eecd4a03ba2d327bee3e5e794732")
 
     def source(self):
         tools.download("https://www.libsdl.org/release/%s" % self.zip_name, self.zip_name)
@@ -36,10 +37,12 @@ class Conan(ConanFile):
         tools.patch(base_path=self.source_subfolder, patch_file="CMakeLists.diff")
 
     def build(self):
+        from cmake_utils import cmake_init, cmake_build_debug_release
         cmake = cmake_init(self.settings, CMake(self), self.build_folder)
-        cmake_build_debug_release(cmake, self.build_subfolder)
+        cmake_build_debug_release(cmake, self.build_subfolder, self.run)
 
     def package(self):
+        from cmake_utils import cmake_init, cmake_install_debug_release
         cmake = cmake_init(self.settings, CMake(self), self.build_folder)
         cmake_install_debug_release(cmake, self.build_subfolder)
         if self.settings.os == "Android":
