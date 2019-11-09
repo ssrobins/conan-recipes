10e61b0d9291e0559e15389321db7f553b8e471a
Steve Robinson <ssrobins@gmail.com>
2019-11-09 14:29:18 -0800

    Get package from modified Linux build environment

--- conanfile.py
+++ conanfile.py
@@ -24,9 +24,9 @@ class Conan(ConanFile):
         self.build_requires.add("gtest/1.10.0#ca07d9d05d12f3bbb091f095e30f7c54cd2920ee")
 
     def requirements(self):
-        self.requires.add("sdl2/2.0.8#e429f599c8c7350ba1edb0e501cfb81c23df0e84")
-        self.requires.add("sdl2_image/2.0.5#cd0b95e6ed9530de98d41edcc2330b80f037934c")
-        self.requires.add("sdl2_ttf/2.0.15#4363f82ab5995ae24441d491826f73db28cbac68")
+        self.requires.add("sdl2/2.0.8#6fbd96a731b885d3316671eb252968e173d48fbc")
+        self.requires.add("sdl2_image/2.0.5#1c6c8f29fe92cfc43ee055200e2bd3d07da06020")
+        self.requires.add("sdl2_ttf/2.0.15#cf879d4de1f7dff57b543a00418a9fa599ea20c5")
 
     def build(self):
         from cmake_utils import cmake_init, cmake_build_debug_release
