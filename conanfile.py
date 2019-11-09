1c6c8f29fe92cfc43ee055200e2bd3d07da06020
Steve Robinson <ssrobins@gmail.com>
2019-11-09 14:22:32 -0800

    Get package from modified Linux build environment

--- conanfile.py
+++ conanfile.py
@@ -22,7 +22,7 @@ class Conan(ConanFile):
 
     def requirements(self):
         self.requires.add("libpng/1.6.37#1c402e5f3dce526ac836af9cdc268d1c40a75f82")
-        self.requires.add("sdl2/2.0.8#e429f599c8c7350ba1edb0e501cfb81c23df0e84")
+        self.requires.add("sdl2/2.0.8#6fbd96a731b885d3316671eb252968e173d48fbc")
 
     def source(self):
         tools.download("https://www.libsdl.org/projects/SDL_image/release/%s" % self.zip_name, self.zip_name)
