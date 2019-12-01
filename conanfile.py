737696be61aa314b900b215d354851f1dec57257
Steve Robinson <ssrobins@gmail.com>
2019-11-30 21:47:30 -0800

    Add Xcode archive to CMake Install and Xcode export to CPack

--- conanfile.py
+++ conanfile.py
@@ -2,7 +2,7 @@ from conans import ConanFile
 
 class Conan(ConanFile):
     name = "cmake_utils"
-    version = "0.1.0"
+    version = "0.1.1"
     description = "Shared CMake utilities"
     license = "MIT"
     url = "https://gitlab.com/ssrobins/conan-" + name
---
+++ package_ios_ipa.cmake
@@ -0,0 +1,32 @@
+function(xcode_package component)
+    if(component)
+        set(package_name ${component})
+    else()
+        get_filename_component(package_name ${CPACK_TEMPORARY_DIRECTORY} NAME)
+    endif()
+
+    message("Building ipa for ${package_name}")
+
+    execute_process(
+        COMMAND xcodebuild -exportArchive -archivePath ${package_name}.xcarchive -exportOptionsPlist export_options.plist -exportPath .
+        WORKING_DIRECTORY ${CPACK_TEMPORARY_DIRECTORY}/${component}
+        RESULT_VARIABLE xcode_package_result
+    )
+    if(xcode_package_result)
+        message(FATAL_ERROR "Xcode error")
+    endif()
+
+    file(GLOB_RECURSE ipa_files
+        "${CPACK_TEMPORARY_DIRECTORY}/${component}/*.ipa"
+    )
+    file(COPY ${ipa_files} DESTINATION ${CPACK_PACKAGE_DIRECTORY})
+endfunction(xcode_package)
+
+
+if(CPACK_COMPONENTS_ALL)
+    foreach(component ${CPACK_COMPONENTS_ALL})
+        xcode_package(${component})
+    endforeach()
+else()
+    xcode_package("")
+endif()
