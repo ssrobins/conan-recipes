02dc0936016e3940b5abfabf3385c6d2ac3b0dcf
Steve Robinson <ssrobins@gmail.com>
2019-12-11 23:01:50 -0800

    Update CMake minimim and use Conan in TARGETS mode

--- conanfile.py
+++ conanfile.py
@@ -21,8 +21,8 @@ class Conan(ConanFile):
         self.build_requires.add("cmake_utils/0.1.0#7f17deeced79eecd4a03ba2d327bee3e5e794732")
     
     def requirements(self):
-        self.requires.add("freetype/2.10.1#478bddfff6d46826c808389cc9af7613add1bc14")
-        self.requires.add("sdl2/2.0.8#5471132184e2acc3e2f1e9ed4a121c4a076cc39e")
+        self.requires.add("freetype/2.10.1#77ace7d6e88ebe28ce9fe12bc4f35917e951f0cb")
+        self.requires.add("sdl2/2.0.8#7654479279c54a466533bce5050df86b4afeb51d")
 
     def source(self):
         tools.download("https://www.libsdl.org/projects/SDL_ttf/release/%s" % self.zip_name, self.zip_name)
