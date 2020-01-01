f8d2b983da21ccdd748add1fe664b3983c531f20
Steve Robinson <ssrobins@gmail.com>
2020-01-01 12:09:27 -0800

    Install Xcode workspace settings that put the DerivedData dir in the project build directory

--- conanfile.py
+++ conanfile.py
@@ -18,7 +18,7 @@ class Conan(ConanFile):
     source_subfolder = "source"
 
     def build_requirements(self):
-        self.build_requires.add("cmake_utils/0.3.0#0ec3922f6b2df47dc695bacdbc5491fb972f0a75")
+        self.build_requires.add("cmake_utils/0.3.1#1cf9333e6fba1b7350ec8d4d06f737b54d163eef")
     
     def requirements(self):
         self.requires.add("sdl2/2.0.8#0ac39a5179e1f92a0af645ee15c70760af8cf590")
