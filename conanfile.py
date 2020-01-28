afbdb345fe6314dcd7881de4174ee68e4b035aa6
Steve Robinson <ssrobins@gmail.com>
2020-01-27 21:01:18 -0800

    Use tools.get instead of tools.download and switch to formatted string literals

--- conanfile.py
+++ conanfile.py
@@ -7,13 +7,13 @@ class Conan(ConanFile):
     description = "Official PNG image format reference library"
     homepage = "http://www.libpng.org"
     license = "Libpng http://www.libpng.org/pub/png/src/libpng-LICENSE.txt"
-    url = "https://gitlab.com/ssrobins/conan-" + name
+    url = f"https://gitlab.com/ssrobins/conan-{name}"
     settings = "os", "compiler", "arch"
     generators = "cmake"
     revision_mode = "scm"
     exports_sources = ["CMakeLists.diff", "CMakeLists.txt"]
-    zip_folder_name = "libpng-%s" % version
-    zip_name = "%s.tar.gz" % zip_folder_name
+    zip_folder_name = f"libpng-{version}"
+    zip_name = f"{zip_folder_name}.tar.gz"
     build_subfolder = "build"
     source_subfolder = "source"
     maj_min_ver = str().join(version.split(".")[0:2])
@@ -25,9 +25,8 @@ class Conan(ConanFile):
         self.requires.add("zlib/1.2.11#75fcaf34fb01804beebfe637a638e9652a3e0f01")
 
     def source(self):
-        tools.download("http://dnqpy.com/libs/%s" % self.zip_name, self.zip_name)
-        tools.unzip(self.zip_name)
-        os.unlink(self.zip_name)
+        tools.get(f"http://dnqpy.com/libs/{self.zip_name}",
+            sha256="daeb2620d829575513e35fecc83f0d3791a620b9b93d800b763542ece9390fb4")
         os.rename(self.zip_folder_name, self.source_subfolder)
 
         # Apply a patch to the libpng CMakeLists.txt file with the following changes:
@@ -49,8 +48,8 @@ class Conan(ConanFile):
 
     def package_info(self):
         if self.settings.os == "Windows":
-            self.cpp_info.debug.libs = ["libpng%s_staticd" % self.maj_min_ver]
-            self.cpp_info.release.libs = ["libpng%s_static" % self.maj_min_ver]
+            self.cpp_info.debug.libs = [f"libpng{self.maj_min_ver}_staticd"]
+            self.cpp_info.release.libs = [f"libpng{self.maj_min_ver}_static"]
         else:
-            self.cpp_info.debug.libs = ["png%sd" % self.maj_min_ver]
-            self.cpp_info.release.libs = ["png%s" % self.maj_min_ver]
+            self.cpp_info.debug.libs = [f"png{self.maj_min_ver}d"]
+            self.cpp_info.release.libs = [f"png{self.maj_min_ver}"]
