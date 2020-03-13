e7f1c1cea199868047cd6f25880ff61f1fb7546b
Steve Robinson <ssrobins@gmail.com>
2020-03-12 17:54:49 -0700

    Use documented calls for build_requires and requires

--- conanfile.py
+++ conanfile.py
@@ -18,11 +18,11 @@ class Conan(ConanFile):
     source_subfolder = "source"
 
     def build_requirements(self):
-        self.build_requires.add("cmake_utils/0.3.1#7b308615a235fdf046db096dd35325c0375c2f88")
+        self.build_requires("cmake_utils/0.3.1#7b308615a235fdf046db096dd35325c0375c2f88")
     
     def requirements(self):
-        self.requires.add("freetype/2.10.1#506979c4a96dcbdab45458072713937afd5ad033")
-        self.requires.add("sdl2/2.0.8#60fdb231f6e74bb622017585003a2c09c82d8b35")
+        self.requires("freetype/2.10.1#506979c4a96dcbdab45458072713937afd5ad033")
+        self.requires("sdl2/2.0.8#60fdb231f6e74bb622017585003a2c09c82d8b35")
 
     def source(self):
         tools.get(f"https://www.libsdl.org/projects/SDL_ttf/release/{self.zip_name}")
