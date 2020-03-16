5553982e10122a4940ad92341b2d5e6e0b39c86b
Steve Robinson <ssrobins@gmail.com>
2020-03-15 23:27:46 -0700

    Upgrade Conan packages so Ninja-built Debug libs get published again

--- conanfile.py
+++ conanfile.py
@@ -18,11 +18,11 @@ class Conan(ConanFile):
     source_subfolder = "source"
 
     def build_requirements(self):
-        self.build_requires("cmake_utils/0.3.1#09e87aa7b71951c0c77bbf861baaaa53c3d55830")
+        self.build_requires("cmake_utils/0.3.1#d093c585be2418d6a664babbec39e71d6b5cd11d")
     
     def requirements(self):
-        self.requires("freetype/2.10.1#35ade12dcdd27a95fc3a27ea2eed5b2210b66126")
-        self.requires("sdl2/2.0.8#51a526f605844c5d5f73275bf3fe1ad121509866")
+        self.requires("freetype/2.10.1#0b3e6d785894831578678a6071b8b51e99eb58ff")
+        self.requires("sdl2/2.0.8#ecad87bc0bf63f851560e29d3b2e0d1881051544")
 
     def source(self):
         tools.get(f"https://www.libsdl.org/projects/SDL_ttf/release/{self.zip_name}")
