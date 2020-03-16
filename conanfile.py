1a5b63e14ea15c351c3f5b3e187336c3fdfc7f7f
Steve Robinson <ssrobins@gmail.com>
2020-03-15 23:31:06 -0700

    Upgrade Conan packages so Ninja-built Debug libs get published again

--- conanfile.py
+++ conanfile.py
@@ -20,13 +20,13 @@ class Conan(ConanFile):
     build_subfolder = "build"
 
     def build_requirements(self):
-        self.build_requires("cmake_utils/0.3.1#09e87aa7b71951c0c77bbf861baaaa53c3d55830")
-        self.build_requires("gtest/1.10.0#e61f09593757c0bc883d927a3b7f284dfb1c0267")
+        self.build_requires("cmake_utils/0.3.1#d093c585be2418d6a664babbec39e71d6b5cd11d")
+        self.build_requires("gtest/1.10.0#1dc34103486deb0c4056a3326c005456f805fc6b")
 
     def requirements(self):
-        self.requires("sdl2/2.0.8#51a526f605844c5d5f73275bf3fe1ad121509866")
-        self.requires("sdl2_image/2.0.5#306817061e8c746beee556b04facaf7ab1dcc585")
-        self.requires("sdl2_ttf/2.0.15#c390ca26f68facafb46d1cf7270c38d647b567c7")
+        self.requires("sdl2/2.0.8#ecad87bc0bf63f851560e29d3b2e0d1881051544")
+        self.requires("sdl2_image/2.0.5#b30afe725bb6b53b7ba3230c75a2ed93fe261591")
+        self.requires("sdl2_ttf/2.0.15#5553982e10122a4940ad92341b2d5e6e0b39c86b")
 
     def build(self):
         from cmake_utils import cmake_init, cmake_build_debug_release
