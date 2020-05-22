63a50a1e81ce6ccf89196114a653fb162bbbca7e
Steve Robinson <ssrobins@gmail.com>
2020-05-21 22:37:46 -0700

    Build with upgraded VS2019 and Xcode

--- conanfile.py
+++ conanfile.py
@@ -18,7 +18,7 @@ class Conan(ConanFile):
     source_subfolder = "source"
 
     def build_requirements(self):
-        self.build_requires("cmake_utils/0.3.1#d093c585be2418d6a664babbec39e71d6b5cd11d")
+        self.build_requires("cmake_utils/0.3.1#b92e3b563e31a4fe0e55849f3bfdb55eb7b06284")
 
     def source(self):
         tools.get(f"https://github.com/google/googletest/archive/{self.zip_name}")
