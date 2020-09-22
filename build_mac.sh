5b8ea9b6ebf5221665e50c50e02605ed3cf6cc90
Steve Robinson <ssrobins@gmail.com>
2020-09-21 20:20:23 -0700

    Upgrade to Xcode 12

--- build_mac.sh
+++ build_mac.sh
@@ -4,4 +4,4 @@ cd $(dirname "$0")
 
 conan remote add bintray-stever https://api.bintray.com/conan/stever/conan --insert --force
 
-conan create --update . -s os.version=10.9 -s compiler.version=11.0
+conan create --update . -s os.version=10.9 -s compiler.version=12.0
