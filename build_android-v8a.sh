5bd93315807d501b61bf09b93635730dd19a83fc
Steve Robinson <ssrobins@gmail.com>
2020-12-17 22:12:57 -0800

    Upgrade Android NDK

--- build_android-v8a.sh
+++ build_android-v8a.sh
@@ -4,4 +4,4 @@ cd $(dirname "$0")
 
 conan remote add bintray-stever https://api.bintray.com/conan/stever/conan --insert --force
 
-conan create --update . -s os=Android -s os.api_level=21 -s arch=armv8 -s compiler=clang -s compiler.version=9 -s compiler.libcxx=libc++
+conan create --update . -s os=Android -s os.api_level=21 -s arch=armv8 -s compiler=clang -s compiler.version=11 -s compiler.libcxx=libc++
