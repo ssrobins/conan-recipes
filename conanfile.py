5896ac21479f7b0d02eafb08b67319bf59cbad17
Steve Robinson <ssrobins@gmail.com>
2020-12-22 23:15:32 -0800

    Upgrade Android NDK

--- conanfile.py
+++ conanfile.py
@@ -21,12 +21,12 @@ class Conan(ConanFile):
 
     def build_requirements(self):
         self.build_requires("cmake_utils/0.3.1#cc144db607f04d12c0b18303a7c7d37386ce0783")
-        self.build_requires("gtest/1.10.0#044b450dd54106b2b6a32a6fa46ba984756f73b6")
+        self.build_requires("gtest/1.10.0#5bd93315807d501b61bf09b93635730dd19a83fc")
 
     def requirements(self):
-        self.requires("sdl2/2.0.8#abbd4c5be105743339ebff6140b9154669b9de00")
-        self.requires("sdl2_image/2.0.5#9f6c9d24e18ab00f7f1efd377b1277b93baa776e")
-        self.requires("sdl2_ttf/2.0.15#1aa0b7ec2f9d33f57b72e31d007b5c86bbbbf274")
+        self.requires("sdl2/2.0.8#f45c74e900f09457e388645638c72802bd4cd081")
+        self.requires("sdl2_image/2.0.5#4e5a6fbdb6d3b6ec042dcb73c5121b119294c94d")
+        self.requires("sdl2_ttf/2.0.15#21c89ad0e5ba664bc7cd317639737ad857476e91")
 
     def build(self):
         from cmake_utils import cmake_init, cmake_build_debug_release
