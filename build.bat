ed934365b888c490f9dca54779d85a6e3cc3cc52
Steve Robinson <ssrobins@gmail.com>
2021-02-14 18:06:12 -0800

    Switch from bintray to artifactory for Conan packages (#2)

--- build.bat
+++ build.bat
@@ -3,6 +3,6 @@ setlocal
 
 cd /d %~dp0
 
-conan remote add bintray-stever https://api.bintray.com/conan/stever/conan --insert --force
+conan remote add artifactory-ssrobins https://ssrobins.jfrog.io/artifactory/api/conan/conan --insert --force
 
 conan create --update .
