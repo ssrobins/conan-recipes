fd869827da9c3bc571dc553e945489ec276e72a6
Steve Robinson <ssrobins@gmail.com>
2021-02-14 20:58:25 -0800

    Switch from bintray to artifactory for Conan packages (#3)

--- conanfile.py
+++ conanfile.py
@@ -22,7 +22,7 @@ class Conan(ConanFile):
     source_subfolder = "source"
 
     def build_requirements(self):
-        self.build_requires("cmake_utils/0.3.1#cc144db607f04d12c0b18303a7c7d37386ce0783")
+        self.build_requires("cmake_utils/0.3.1#e474aafdec36cf92d97e781b844f390f3170f29f")
 
     def source(self):
         tools.get(f"https://zlib.net/{self.zip_name}",
