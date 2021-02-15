931e89a2eedabf1f4b975d4207565e8b3f394866
Steve Robinson <ssrobins@gmail.com>
2021-02-14 21:41:30 -0800

    Switch from bintray to artifactory for Conan packages

--- build_mac.sh
+++ build_mac.sh
@@ -2,6 +2,6 @@ set -e
   
 cd $(dirname "$0")
 
-conan remote add bintray-stever https://api.bintray.com/conan/stever/conan --insert --force
+conan remote add artifactory-ssrobins https://ssrobins.jfrog.io/artifactory/api/conan/conan --insert --force
 
 conan create --update . -s os.version=10.9 -s compiler.version=12.0
