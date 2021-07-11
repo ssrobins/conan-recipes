0f6e34b34f9f3572720069184d50800ef1130aa0
Steve Robinson <ssrobins@gmail.com>
2021-07-11 13:20:09 -0700

    Stop setting build_type in Conan and make sure all other settings match the app's settings exactly

--- conan.py
+++ conan.py
@@ -11,29 +11,23 @@ def main():
         "androidarm": "-s os=Android -s os.api_level=16 -s arch=armv7 -s compiler=clang -s compiler.version=11 -s compiler.libcxx=c++_static",
         "androidarm64": "-s os=Android -s os.api_level=21 -s arch=armv8 -s compiler=clang -s compiler.version=11 -s compiler.libcxx=c++_static",
         "ios": "-s os=iOS -s arch=armv7 -s os.version=9.0 -s compiler.version=12.0",
-        "linux": "",
+        "linux": "-s compiler=gcc -s compiler.version=11",
         "macos": "-s os.version=10.9 -s compiler.version=12.0",
         "windows": "-s arch=x86 -s compiler.version=16 -s compiler.runtime=MT"
     }
 
     parser = argparse.ArgumentParser()
     parser.add_argument("platform", choices=list(platform.keys()), help="Build platform")
-    parser.add_argument("--config", help="Build config")
     command_args = parser.parse_args()
 
     script_path = os.path.dirname(os.path.realpath(__file__))
-
-    if command_args.config:
-        config = f"-s build_type={command_args.config}"
-    else:
-        config = "-s build_type=Debug"
 
     remote_url = "https://ssrobins.jfrog.io/artifactory/api/conan/conan"
     conan_remote = f"conan remote add artifactory-ssrobins {remote_url} --insert --force"
     print(conan_remote, flush=True)
     subprocess.run(conan_remote, cwd=script_path, shell=True, check=True)
 
-    conan_create = f"conan create --update . {platform[command_args.platform]} {config}"
+    conan_create = f"conan create --update . {platform[command_args.platform]}"
     print(conan_create, flush=True)
     subprocess.run(conan_create, cwd=script_path, shell=True, check=True)
 
