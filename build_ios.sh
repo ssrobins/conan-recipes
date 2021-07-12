d19b1126454ef7aad4555b680e5c6aed0349f17f
Steve Robinson <ssrobins@gmail.com>
2021-07-11 20:17:21 -0700

    Consolidate Conan scripts

--- build_ios.sh
+++
@@ -1,7 +0,0 @@
-set -e
-
-cd $(dirname "$0")
-
-conan remote add artifactory-ssrobins https://ssrobins.jfrog.io/artifactory/api/conan/conan --insert --force
-
-conan create --update . -s os=iOS -s arch=armv7 -s os.version=9.0 -s compiler.version=12.0
