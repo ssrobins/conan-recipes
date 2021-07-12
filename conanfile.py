496f9c0ffbc398ada023789d248e1011abc0d79f
Steve Robinson <ssrobins@gmail.com>
2021-07-11 20:31:34 -0700

    Consolidate Conan scripts

--- conanfile.py
+++ conanfile.py
@@ -21,12 +21,12 @@ class Conan(ConanFile):
 
     def build_requirements(self):
         self.build_requires("cmake_utils/0.3.1#77d5f06b9b20302a5410e41ed45e7bbea7de90a5")
-        self.build_requires("gtest/1.11.0#1b8ec1a4d85a679be150cd3ad2dad866ca2aab30")
+        self.build_requires("gtest/1.11.0#e16a50c3a5d1fcb9955b759f6b6d82dea712d821")
 
     def requirements(self):
-        self.requires("sdl2/2.0.14#7d63f39c4feb6f6922d7e88c834c2fbfd9d586c7")
-        self.requires("sdl2_image/2.0.5#6c8e080bd3d4e55cdf964a3cdcb23f9ea8dbac85")
-        self.requires("sdl2_ttf/2.0.15#7954c9646cdb1083de53677c8a8733c64123a4e5")
+        self.requires("sdl2/2.0.14#ce9213846a95e551ab180eaf53ca8c44c6788b91")
+        self.requires("sdl2_image/2.0.5#d19b1126454ef7aad4555b680e5c6aed0349f17f")
+        self.requires("sdl2_ttf/2.0.15#a059935288591335fe4cb0bd1866fe7a7acf3bbb")
 
     def build(self):
         from cmake_utils import cmake_init, cmake_build_debug_release
