d7d920cc764dfe4a64a9e978e627a588d0c16cfa
Steve Robinson <ssrobins@gmail.com>
2021-10-02 18:18:26 -0700

    Build Linux with Ubuntu 18 for broader compatibility

--- conanfile.py
+++ conanfile.py
@@ -20,13 +20,13 @@ class Conan(ConanFile):
     build_subfolder = "build"
 
     def build_requirements(self):
-        self.build_requires("cmake_utils/0.3.1#77d5f06b9b20302a5410e41ed45e7bbea7de90a5")
-        self.build_requires("gtest/1.11.0#5c7586ef6610e8053ee79086fbc7743d9fbde653")
+        self.build_requires("cmake_utils/0.3.1#a1d53d179d9736ff032b5f5de3e4c3e2eebcb1f0")
+        self.build_requires("gtest/1.11.0#42289b3e08c69030c7f755a4d49d0b0b4d78103e")
 
     def requirements(self):
-        self.requires("sdl2/2.0.16#b5081764744dd13a648d8ae2ff0d0abce3d7fe91")
-        self.requires("sdl2_image/2.0.5#b5d3918c53fdd0f98369d62a030e62d155645891")
-        self.requires("sdl2_ttf/2.0.15#9ea05bfcaf8e9d6819d67f24db6a3d2300914011")
+        self.requires("sdl2/2.0.16#d64fb9b295748eeef2db30c50a8aa6d3f948f770")
+        self.requires("sdl2_image/2.0.5#22cbb9c463ce625b4c878532fd4d19e66e6442ad")
+        self.requires("sdl2_ttf/2.0.15#bfda6aa5591c9a6b8dfeb88a8147f539e087ce48")
 
     def build(self):
         from cmake_utils import cmake_init, cmake_build_debug_release
