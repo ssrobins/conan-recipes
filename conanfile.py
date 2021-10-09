8f04c86eafae2ff0e33063296233d79e48c9a701
Steve Robinson <ssrobins@gmail.com>
2021-10-09 00:58:22 -0700

    Upgrade Xcode

--- conanfile.py
+++ conanfile.py
@@ -21,12 +21,12 @@ class Conan(ConanFile):
 
     def build_requirements(self):
         self.build_requires("cmake_utils/0.3.1#a1d53d179d9736ff032b5f5de3e4c3e2eebcb1f0")
-        self.build_requires("gtest/1.11.0#42289b3e08c69030c7f755a4d49d0b0b4d78103e")
+        self.build_requires("gtest/1.11.0#da27a76e3c6d8034aee23541c0c4e9720f224ecc")
 
     def requirements(self):
-        self.requires("sdl2/2.0.16#d64fb9b295748eeef2db30c50a8aa6d3f948f770")
-        self.requires("sdl2_image/2.0.5#22cbb9c463ce625b4c878532fd4d19e66e6442ad")
-        self.requires("sdl2_ttf/2.0.15#bfda6aa5591c9a6b8dfeb88a8147f539e087ce48")
+        self.requires("sdl2/2.0.16#e48e0f700a2932b3c19dcc83fac49275cf8d5efc")
+        self.requires("sdl2_image/2.0.5#d22c1f7caea43b4a632b80b0595055aad5e305bd")
+        self.requires("sdl2_ttf/2.0.15#9c52fbd7a761e88d01ed6ff541ff83904363d5a8")
 
     def build(self):
         from cmake_utils import cmake_init, cmake_build_debug_release
