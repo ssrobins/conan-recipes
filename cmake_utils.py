4e7b4d9bfca394477325cdfc8eacce8b1c82583e
Steve Robinson <ssrobins@gmail.com>
2021-11-21 11:18:36 -0800

    Use Ninja to build macOS and iOS packages

--- cmake_utils.py
+++ cmake_utils.py
@@ -2,8 +2,8 @@ import os, shutil
 from conans import tools
 
 def cmake_init(settings, cmake, build_folder):
+    cmake.generator = "Ninja Multi-Config"
     if settings.os == "Android":
-        cmake.generator = "Ninja Multi-Config"
         if settings.arch == "armv7":
             cmake.definitions["ANDROID_ABI"] = "armeabi-v7a"
         elif settings.arch == "armv8":
@@ -12,19 +12,15 @@ def cmake_init(settings, cmake, build_folder):
         cmake.definitions["ANDROID_STL"] = "c++_static"
         cmake.definitions["CMAKE_TOOLCHAIN_FILE"] = os.getenv("ANDROID_NDK_ROOT") + "/build/cmake/android.toolchain.cmake"
     elif settings.os == "iOS":
-        cmake.generator = "Xcode"
         cmake.definitions["CMAKE_SYSTEM_NAME"] = "iOS"
         if settings.os.version:
             cmake.definitions["CMAKE_OSX_DEPLOYMENT_TARGET"] = settings.os.version
         if settings.arch == "x86_64":
             cmake.definitions["CMAKE_OSX_ARCHITECTURES"] = "x86_64"
         else:
-            cmake.definitions["CMAKE_OSX_ARCHITECTURES"] = "armv7 arm64"
+            cmake.definitions["CMAKE_OSX_ARCHITECTURES"] = "armv7;arm64"
         cmake.definitions["CMAKE_TRY_COMPILE_TARGET_TYPE"] = "STATIC_LIBRARY"
-    elif settings.os == "Linux":
-        cmake.generator = "Ninja Multi-Config"
     elif settings.os == "Macos":
-        cmake.generator = "Xcode"
         if settings.os.version:
             cmake.definitions["CMAKE_OSX_DEPLOYMENT_TARGET"] = settings.os.version
     elif settings.os == "Windows":
