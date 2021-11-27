990c9e26dd59ee78d2fe5d1c3e2f1c544afe1dfd
Steve Robinson <ssrobins@gmail.com>
2021-11-26 18:06:27 -0800

    Fix error when upgrading to Android target SDK to 31

--- conanfile.py
+++ conanfile.py
@@ -2,7 +2,7 @@ from conans import ConanFile
 
 class Conan(ConanFile):
     name = "android_sdl2"
-    version = "2.0.0"
+    version = "2.1.0"
     description = "Android Gradle project for SDL2"
     license = "MIT"
     url = f"https://github.com/ssrobins/conan-{name}"
