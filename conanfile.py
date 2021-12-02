efe16f5cd79ba2abe4aaa839d189f858f22f952d
Steve Robinson <ssrobins@gmail.com>
2021-12-01 21:56:08 -0800

    Upgrade SDL2

--- conanfile.py
+++ conanfile.py
@@ -2,7 +2,7 @@ from conans import ConanFile, CMake
 
 class Conan(ConanFile):
     name = "ssrobins_engine"
-    version = "1.0.0"
+    version = "1.1.0"
     description = "Thin game engine wrapper"
     homepage = f"https://github.com/ssrobins/conan-{name}"
     license = "MIT"
@@ -20,13 +20,13 @@ class Conan(ConanFile):
     build_subfolder = "build"
 
     def build_requirements(self):
-        self.build_requires("cmake_utils/3.0.0#4e7b4d9bfca394477325cdfc8eacce8b1c82583e")
+        self.build_requires("cmake_utils/5.0.0#1ecfed8c68a43ea17d321701cc8a91df21d06453")
         self.build_requires("gtest/1.11.0#7a8dc4022cf1753b71057bfe89043cf4a5b4dc8f")
 
     def requirements(self):
-        self.requires("sdl2/2.0.16#9092984606ffc5911f079d363886bb8266518b40")
-        self.requires("sdl2_image/2.0.5#04347c0e543b3f9f66f3ea37d26b204dd26b8552")
-        self.requires("sdl2_ttf/2.0.15#9edc8ef3817d264846c6fc2cf8a8d472f164764f")
+        self.requires("sdl2/2.0.18#1b637c42e56a9747a74c2bdeb53370c0cd24c414")
+        self.requires("sdl2_image/2.0.5#a5bb813d197df422cc8d9034adfc7a866f47b42d")
+        self.requires("sdl2_ttf/2.0.15#d5f3a47390d23aba76e992030674d316479acfa7")
 
     def build(self):
         from cmake_utils import cmake_init, cmake_build_debug_release
