d1cb042e463dd5107bf96584e8ced5e1ffa0c5fa
Steve Robinson <ssrobins@gmail.com>
2022-05-23 18:22:22 -0700

    Build separate Debug and Release Conan packages

--- conanfile.py
+++ conanfile.py
@@ -10,10 +10,11 @@ class Conan(ConanFile):
     homepage = "https://zlib.net/"
     license = "Zlib"
     url = f"https://github.com/ssrobins/conan-{name}"
-    settings = "os", "compiler", "arch"
+    settings = "os", "arch", "compiler", "build_type"
     options = {"shared": [True, False]}
     default_options = "shared=False"
     generators = "cmake"
+    _cmake = None
     revision_mode = "scm"
     exports_sources = ["CMakeLists.diff", "CMakeLists.txt"]
     zip_folder_name = f"{name}-{version}"
@@ -22,17 +23,34 @@ class Conan(ConanFile):
     source_subfolder = "source"
 
     def build_requirements(self):
-        self.build_requires("cmake_utils/6.1.0#9ced9bcfd95178b35d2ec5955b725a5652dbda26")
+        self.build_requires("cmake_utils/7.0.0#9bf47716aeee70a8dcfc8592831a0318eb327a09")
 
     def source(self):
         tools.get(f"https://zlib.net/{self.zip_name}",
             sha256="91844808532e5ce316b3c010929493c0244f3d37593afd6de04f71821d5136d9")
         os.rename(self.zip_folder_name, self.source_subfolder)
 
+    def _configure_cmake(self):
+        if self._cmake:
+            return self._cmake
+        self._cmake = CMake(self)
+        if self.settings.os != "Windows":
+            self._cmake.generator = "Ninja Multi-Config"
+        if self.settings.os == "Android":
+            self._cmake.definitions["CMAKE_TOOLCHAIN_FILE"] = os.getenv("ANDROID_NDK_ROOT") + "/build/cmake/android.toolchain.cmake"
+        elif self.settings.os == "iOS" and self.settings.arch != "x86_64":
+            self._cmake.definitions["CMAKE_OSX_ARCHITECTURES"] = "armv7;arm64"
+        elif self.settings.os == "Windows" and self.settings.arch == "x86":
+            self._cmake.generator_platform = "Win32"
+        self._cmake.configure(build_dir=self.build_subfolder)
+        return self._cmake
+
     def build(self):
-        from cmake_utils import cmake_init, cmake_build_debug_release
-        cmake = cmake_init(self.settings, CMake(self), self.build_folder)
-        cmake_build_debug_release(cmake, self.build_subfolder, self.run)
+        cmake = self._configure_cmake()
+        cmake.build(args=["--verbose"])
+        with tools.chdir(self.build_subfolder):
+            self.run(f"ctest -C {self.settings.build_type} --output-on-failure")
+        cmake.install()
 
     def package(self):
         self.copy("*.h", dst="include", src=self.source_subfolder)
@@ -44,8 +62,12 @@ class Conan(ConanFile):
 
     def package_info(self):
         if self.settings.os == "Windows" and not tools.os_info.is_linux:
-            self.cpp_info.debug.libs = ["zlibstaticd"]
-            self.cpp_info.release.libs = ["zlibstatic"]
+            if self.settings.build_type == "Debug":
+                self.cpp_info.libs = ["zlibstaticd"]
+            else:
+                self.cpp_info.libs = ["zlibstatic"]
         else:
-            self.cpp_info.debug.libs = ["zd"]
-            self.cpp_info.release.libs = ["z"]
+            if self.settings.build_type == "Debug":
+                self.cpp_info.libs = ["zd"]
+            else:
+                self.cpp_info.libs = ["z"]
