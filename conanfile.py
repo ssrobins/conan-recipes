08f602c34c374893ef75d96a088135f05fd255fa
Steve Robinson <ssrobins@gmail.com>
2022-05-23 23:30:43 -0700

    Build separate Debug and Release Conan packages

--- conanfile.py
+++ conanfile.py
@@ -8,8 +8,9 @@ class Conan(ConanFile):
     homepage = "https://www.libsdl.org/projects/SDL_mixer/"
     license = "Zlib"
     url = f"https://github.com/ssrobins/conan-{name}"
-    settings = "os", "compiler", "arch"
+    settings = "os", "arch", "compiler", "build_type"
     generators = "cmake"
+    _cmake = None
     revision_mode = "scm"
     exports_sources = ["CMakeLists.txt", f"CMakeLists-{name}.txt"]
     zip_folder_name = f"SDL2_mixer-{version}"
@@ -18,21 +19,37 @@ class Conan(ConanFile):
     source_subfolder = "source"
 
     def build_requirements(self):
-        self.build_requires("cmake_utils/5.1.0#2c0d8f9dda3cac137976849bb3851fd6c4999de0")
+        self.build_requires("cmake_utils/7.0.0#9bf47716aeee70a8dcfc8592831a0318eb327a09")
     
     def requirements(self):
-        self.requires("sdl2/2.0.20#45420ef8e58422639bfab3f61e40d75a03091154")
-        self.requires("vorbis/1.3.7#6c0082787f1881f6c8a589304b45c20014a83ba9")
+        self.requires("sdl2/2.0.22#9eef18bc748aef7bfc89e085ee925b18e60741c6")
+        self.requires("vorbis/1.3.7#6cb3266a1766ef5b119ade3fd4ccd5cae4093c48")
 
     def source(self):
         tools.get(f"https://www.libsdl.org/projects/SDL_mixer/release/{self.zip_name}")
         os.rename(self.zip_folder_name, self.source_subfolder)
         shutil.move(f"CMakeLists-{self.name}.txt", os.path.join(self.source_subfolder, "CMakeLists.txt"))
 
+    def _configure_cmake(self):
+        if self._cmake:
+            return self._cmake
+        self._cmake = CMake(self)
+        if self.settings.os != "Windows":
+            self._cmake.generator = "Ninja Multi-Config"
+        if self.settings.os == "Android":
+            self._cmake.definitions["CMAKE_TOOLCHAIN_FILE"] = os.getenv("ANDROID_NDK_ROOT") + "/build/cmake/android.toolchain.cmake"
+        elif self.settings.os == "iOS" and self.settings.arch != "x86_64":
+            self._cmake.definitions["CMAKE_OSX_ARCHITECTURES"] = "armv7;arm64"
+        elif self.settings.os == "Windows" and self.settings.arch == "x86":
+            self._cmake.generator_platform = "Win32"
+        self._cmake.configure(build_dir=self.build_subfolder)
+        return self._cmake
+
     def build(self):
-        from cmake_utils import cmake_init, cmake_build_debug_release
-        cmake = cmake_init(self.settings, CMake(self), self.build_folder)
-        cmake_build_debug_release(cmake, self.build_subfolder, self.run)
+        cmake = self._configure_cmake()
+        cmake.build(args=["--verbose"])
+        with tools.chdir(self.build_subfolder):
+            self.run(f"ctest -C {self.settings.build_type} --output-on-failure")
 
     def package(self):
         self.copy("SDL_mixer.h", dst="include", src=self.source_subfolder)
@@ -42,5 +59,7 @@ class Conan(ConanFile):
             self.copy("*.pdb", dst="lib", keep_path=False)
 
     def package_info(self):
-        self.cpp_info.debug.libs = ["SDL2_mixerd"]
-        self.cpp_info.release.libs = ["SDL2_mixer"]
+        if self.settings.build_type == "Debug":
+            self.cpp_info.libs = ["SDL2_mixerd"]
+        else:
+            self.cpp_info.libs = ["SDL2_mixer"]
