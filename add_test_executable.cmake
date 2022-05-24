b0932588b4ccd8ac8db4fed90295f7480ac0b37f
Steve Robinson <ssrobins@gmail.com>
2022-05-24 09:01:33 -0700

    Make gtest target compatible with Conan's generators that use find_package()

--- add_test_executable.cmake
+++ add_test_executable.cmake
@@ -2,7 +2,7 @@ function(add_test_executable target_name)
     add_executable_custom(${target_name})
 
     target_link_libraries(${target_name} 
-        CONAN_PKG::gtest
+        gtest::gtest
     )
 
     if(NOT ANDROID AND NOT IOS)
