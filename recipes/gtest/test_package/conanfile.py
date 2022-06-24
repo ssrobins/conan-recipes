3bf4f96244264652925efcc5e805fa72ec1ea37c
Steve Robinson <ssrobins@gmail.com>
2022-06-23 22:48:10 -0700

    Upgrade gtest

--- recipes/gtest/test_package/conanfile.py
+++ recipes/gtest/test_package/conanfile.py
@@ -7,7 +7,7 @@ class TestPackageConan(ConanFile):
     generators = "CMakeDeps"
 
     def requirements(self):
-        self.requires("gtest/1.11.0")
+        self.requires("gtest/1.12.0")
 
     def generate(self):
         tc = CMakeToolchain(self)
