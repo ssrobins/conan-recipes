e6181ac1f01566b9a538c4d64eafd54c8b420188
Steve Robinson <ssrobins@gmail.com>
2022-07-09 23:41:53 -0700

    Fixes

--- recipes/ssrobins_engine/conanfile.py
+++ recipes/ssrobins_engine/conanfile.py
@@ -7,7 +7,7 @@ required_conan_version = ">=2.0.0-beta1"
 
 class Conan(ConanFile):
     name = "ssrobins_engine"
-    version = "1.2.0"
+    version = "1.3.0"
     description = "Thin game engine wrapper"
     homepage = "https://github.com/ssrobins/conan-recipes"
     license = "MIT"
