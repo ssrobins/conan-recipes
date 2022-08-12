800c8f70d099bcb65471cafb14efdc2540d932f3
Steve Robinson <ssrobins@gmail.com>
2022-08-11 22:14:22 -0700

    Fix Android APK copy and upgrade Gradle

--- recipes/sfml/conanfile.py
+++ recipes/sfml/conanfile.py
@@ -34,7 +34,7 @@ class Conan(ConanFile):
             update=True, check=True)
 
     def requirements(self):
-        self.requires("cmake_utils/10.0.1@ssrobins")
+        self.requires("cmake_utils/10.0.2@ssrobins")
         self.requires("freetype/2.12.1@ssrobins")
 
     @property
