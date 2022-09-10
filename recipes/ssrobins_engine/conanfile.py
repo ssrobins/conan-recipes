d480282223c503ef444c115989d52afcbf48c638
Steve Robinson <ssrobins@gmail.com>
2022-09-10 12:28:35 -0700

    Iterate package version

--- recipes/ssrobins_engine/conanfile.py
+++ recipes/ssrobins_engine/conanfile.py
@@ -7,7 +7,7 @@ required_conan_version = ">=2.0.0-beta1"
 
 class Conan(ConanFile):
     name = "ssrobins_engine"
-    version = "2.0.1"
+    version = "2.0.2"
     description = "Thin game engine wrapper"
     homepage = "https://github.com/ssrobins/conan-recipes"
     license = "MIT"
