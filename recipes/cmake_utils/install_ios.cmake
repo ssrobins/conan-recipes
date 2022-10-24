9b38cdef9fe12cb0e95252994b66d81fd1d53537
Steve Robinson <ssrobins@gmail.com>
2022-10-23 20:12:52 -0700

    Print xcodebuild command

--- recipes/cmake_utils/install_ios.cmake
+++ recipes/cmake_utils/install_ios.cmake
@@ -1,7 +1,8 @@
 execute_process(
     COMMAND xcodebuild -workspace @CMAKE_BINARY_DIR@/@PROJECT_NAME@.xcodeproj/project.xcworkspace
         -scheme @target_name@ archive -archivePath @component_name@.xcarchive
-        RESULT_VARIABLE xcode_archive_result
+    COMMAND_ECHO STDOUT
+    RESULT_VARIABLE xcode_archive_result
     WORKING_DIRECTORY @CMAKE_CURRENT_BINARY_DIR@
     )
     if(xcode_archive_result)
