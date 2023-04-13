7fa5ffc720b7ab58838406da70dedd4c5d07bc0f
Steve Robinson <ssrobins@gmail.com>
2023-04-12 23:19:49 -0700

    Upgrade Gradle

--- recipes/android_sdl/conanfile.py
+++ recipes/android_sdl/conanfile.py
@@ -5,7 +5,7 @@ required_conan_version = ">=2.0.4"
 
 class Conan(ConanFile):
     name = "android_sdl"
-    version = "2.7.5"
+    version = "2.7.6"
     description = "Android Gradle project for SDL2"
     license = "MIT"
     url = "https://github.com/ssrobins/conan-recipes"
